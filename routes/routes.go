e8daefb1e6c5802c90eef70c87781f361cf10e6a
railander soares alcantara dos reis <railanderreis@railanderReis.local>
2023-09-24 12:36:23 -0300

    add edit and delete

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,11 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonaliades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriarUmNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
