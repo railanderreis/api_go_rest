e9a12a859dc1b90edc0c72f2cfa34c439d025b9f
railander soares alcantara dos reis <railanderreis@railanderReis.local>
2023-09-24 11:23:07 -0300

    add mux, routes and image docker

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 
 	models.Personalidades = []models.Personalidade{
-		{Nome: "nome 1", Historia: "historia 1"},
-		{Nome: "nome 2", Historia: "historia 2"},
+		{ID: 1, Nome: "nome 1", Historia: "historia 1"},
+		{ID: 2, Nome: "nome 2", Historia: "historia 2"},
 	}
 
 	fmt.Println("Iniciando o servidor Rest com Go")
--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,7 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/personalidades", controllers.TodasPersonaliades)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonaliades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
