3e7f5d57d5c00f9765819bbab9fbea6a99a18c31
Railander Soares Alcantara dos Reis - TI Infraestrutura <railander.reis@redemeridional.com.br>
2023-09-25 12:49:37 -0300

    adicionado os middleware e cors

--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module github.com/railanderreis/go-rest-api
 go 1.19
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/postgres v1.5.2
+	gorm.io/gorm v1.25.4
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
@@ -11,6 +18,4 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	gorm.io/driver/postgres v1.5.2 // indirect
-	gorm.io/gorm v1.25.4 // indirect
 )
--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/railanderreis/go-rest-api/controllers"
+	"github.com/railanderreis/go-rest-api/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonaliades).Methods("Get")
@@ -17,5 +20,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades", controllers.CriarUmNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
